5a11eb1a9f0bbf72250703fcdd93c027a069c4da
karthikReddy0109 <rangannagarikarthik1977@gmail.com>
2026-08-03 21:26:17 +0530

    Final commit

--- force-app/main/default/lwc/duplicateAccountDetector/duplicateAccountDetector.js
+++ force-app/main/default/lwc/duplicateAccountDetector/duplicateAccountDetector.js
@@ -1,8 +1,42 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from "lwc";
+import findAccount from "@salesforce/apex/AccountController.findAccount";
+import { FlowNavigationNextEvent } from "lightning/flowSupport";
 
 export default class DuplicateAccountDetector extends LightningElement {
-    @api companyName;
-    connectedCallback(){
-        console.log('Company Name : ', this.companyName);
+  @api companyName;
+  @api availableActions = [];
+  connectedCallback() {
+    console.log("Company Name : ", this.companyName);
+  }
+  duplicateFound = false;
+  proceedAnyway = false;
+  errorMessage;
+  @wire(findAccount, { accountName: "$companyName" })
+  wiredDuplicate({ data, error }) {
+    if (data) {
+      this.duplicateFound = data.isSuccess && data.isDuplicate;
+      this.proceedAnyway = false;
+      this.errorMessage = this.duplicateFound ? "Duplicate Account found." : "";
     }
+  }
+  @api
+  validate() {
+    if (this.duplicateFound && !this.proceedAnyway) {
+      return {
+        isValid: false,
+        errorMessage: this.errorMessage
+      };
+    }
+
+    return {
+      isValid: true
+    };
+  }
+
+  handleProceedAnyway() {
+    if (this.availableActions.includes("NEXT")) {
+      this.proceedAnyway = true;
+      this.dispatchEvent(new FlowNavigationNextEvent());
+    }
+  }
 }
